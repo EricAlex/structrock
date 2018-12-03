b656f72594c86366b858fa0688acda2dab9696eb
Xin Wang <WXGHolmes@Gmail.com>
2018-12-02 20:57:17 -0800

    Improved MultiStation

--- MultiStationWorker.cpp
+++ MultiStationWorker.cpp
@@ -43,14 +43,19 @@
 #include <pcl/surface/mls.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/registration/icp.h>
+#include <pcl/registration/icp_nl.h>
+#include <pcl/registration/transforms.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/normal_3d_omp.h>
 #include "globaldef.h"
 #include "MultiStationWorker.h"
 #include "dataLibrary.h"
 
 bool MultiStationWorker::is_para_satisfying(QString message)
 {
-	this->setParaSize(3);
-	if(dataLibrary::Workflow[dataLibrary::current_workline_index].parameters.size()>2)
+	this->setParaSize(6);
+	if(dataLibrary::Workflow[dataLibrary::current_workline_index].parameters.size()>=this->getParaSize())
 	{
 		std::istringstream line_stream(dataLibrary::Workflow[dataLibrary::current_workline_index].parameters[0]);
 		char command_split_str = '|';
@@ -71,13 +76,23 @@ bool MultiStationWorker::is_para_satisfying(QString message)
 			{
 				this->multiStationFilePath.push_back(tokens[i]);
 			}
-			double stdDev, leaf;
-			std::stringstream ss_stdDev(dataLibrary::Workflow[dataLibrary::current_workline_index].parameters[1]);
-			ss_stdDev >> stdDev;
-			this->setStdDev(stdDev);
-			std::stringstream ss_leaf(dataLibrary::Workflow[dataLibrary::current_workline_index].parameters[2]);
-			ss_leaf >> leaf;
-			this->setLeaf(leaf);
+			double pre_align_ds_leaf, pre_align_StdDev, max_correspondence_distance, euclidean_fitness_epsilon;
+			int pre_align_normals_k;
+			std::stringstream ss_pre_align_ds_leaf(dataLibrary::Workflow[dataLibrary::current_workline_index].parameters[1]);
+			ss_pre_align_ds_leaf >> pre_align_ds_leaf;
+			this->setPreAlignDSLeaf(pre_align_ds_leaf);
+			std::stringstream ss_pre_align_StdDev(dataLibrary::Workflow[dataLibrary::current_workline_index].parameters[2]);
+			ss_pre_align_StdDev >> pre_align_StdDev;
+			this->setPreAlignStdDev(pre_align_StdDev);
+			std::stringstream ss_pre_align_normals_k(dataLibrary::Workflow[dataLibrary::current_workline_index].parameters[3]);
+			ss_pre_align_normals_k >> pre_align_normals_k;
+			this->setPreAlignNormalsK(pre_align_normals_k);
+			std::stringstream ss_max_correspondence_distance(dataLibrary::Workflow[dataLibrary::current_workline_index].parameters[4]);
+			ss_max_correspondence_distance >> max_correspondence_distance;
+			this->setMaxCorrDistance(max_correspondence_distance);
+			std::stringstream ss_euclidean_fitness_epsilon(dataLibrary::Workflow[dataLibrary::current_workline_index].parameters[5]);
+			ss_euclidean_fitness_epsilon >> euclidean_fitness_epsilon;
+			this->setEFEpsilon(euclidean_fitness_epsilon);
 
 			this->setParaIndex(this->getParaSize());
 			return true;
@@ -90,7 +105,7 @@ bool MultiStationWorker::is_para_satisfying(QString message)
 	}
 	else
 	{
-		message = QString("multistation: MultiStation Point Cloud File Paths and/or Remove Outliers 'stdDev' and/or Downsample 'Leaf' Not Provided.");
+		message = QString("multistation: MultiStation Point Cloud File Paths Not Provided and/or Not Enough Parameters Given.");
 		return false;
 	}
 }
@@ -130,50 +145,66 @@ void MultiStationWorker::doWork()
 	}
 	if(is_reading_success)
 	{
-		pcl::PointCloud<pcl::PointXYZRGB>::Ptr mid_rgb_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-		pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
-		sor.setInputCloud(this->multiStationPointClouds[0]);
-		sor.setMeanK(50);
-		sor.setStddevMulThresh(this->getStdDev());
-		sor.setNegative(false);
-		sor.filter(*mid_rgb_cloud);
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr dsed_rgb_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr ro_rgb_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+		pcl::PointCloud<pcl::Normal>::Ptr target_normals (new pcl::PointCloud<pcl::Normal>);
+		pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr target_pointnormal (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
 		if(!dataLibrary::cloudxyzrgb->empty())
 		{
 			dataLibrary::cloudxyzrgb->clear();
 		}
-		if(this->getLeaf() == 0.0)
-		{
-			*dataLibrary::cloudxyzrgb = *mid_rgb_cloud;
-		}
-		else
-		{
-			pcl::VoxelGrid<pcl::PointXYZRGB> vg;
-			vg.setInputCloud (mid_rgb_cloud);
-			vg.setLeafSize (this->getLeaf(), this->getLeaf(), this->getLeaf());
-			vg.filter (*dataLibrary::cloudxyzrgb);
-		}
+		*dataLibrary::cloudxyzrgb = *this->multiStationPointClouds[0];
+		pcl::VoxelGrid<pcl::PointXYZRGB> vg;
+		vg.setInputCloud (this->multiStationPointClouds[0]);
+		vg.setLeafSize (this->getPreAlignDSLeaf(), this->getPreAlignDSLeaf(), this->getPreAlignDSLeaf());
+		vg.filter (*dsed_rgb_cloud);
+		pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
+    	sor.setInputCloud(dsed_rgb_cloud);
+    	sor.setMeanK(50);
+    	sor.setStddevMulThresh(this->getPreAlignStdDev());
+		sor.setNegative(false);
+    	sor.filter(*ro_rgb_cloud);
+		pcl::NormalEstimationOMP<pcl::PointXYZRGB, pcl::Normal> ne_target;
+    	ne_target.setInputCloud(ro_rgb_cloud);
+    	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree_target (new pcl::search::KdTree<pcl::PointXYZRGB>());
+    	ne_target.setSearchMethod(tree_target);
+    	ne_target.setKSearch(this->getPreAlignNormalsK());
+    	ne_target.compute(*target_normals);
+		pcl::concatenateFields(*ro_rgb_cloud, *target_normals, *target_pointnormal);
 		for(int i=1; i<this->multiStationPointClouds.size(); i++)
 		{
-			pcl::PointCloud<pcl::PointXYZRGB>::Ptr mid_rgb_cloud_i(new pcl::PointCloud<pcl::PointXYZRGB>);
 			pcl::PointCloud<pcl::PointXYZRGB>::Ptr dsed_rgb_cloud_i(new pcl::PointCloud<pcl::PointXYZRGB>);
+			pcl::PointCloud<pcl::PointXYZRGB>::Ptr ro_rgb_cloud_i(new pcl::PointCloud<pcl::PointXYZRGB>);
+			pcl::PointCloud<pcl::Normal>::Ptr src_normals (new pcl::PointCloud<pcl::Normal>);
+			pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr src_pointnormal (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
+			pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr transformed_pointnormal (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
+			pcl::PointCloud<pcl::PointXYZRGB>::Ptr transformed_rgb_cloud_i(new pcl::PointCloud<pcl::PointXYZRGB>);
+			pcl::VoxelGrid<pcl::PointXYZRGB> vg_i;
+			vg_i.setInputCloud (this->multiStationPointClouds[i]);
+			vg_i.setLeafSize (this->getPreAlignDSLeaf(), this->getPreAlignDSLeaf(), this->getPreAlignDSLeaf());
+			vg_i.filter (*dsed_rgb_cloud_i);
 			pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor_i;
-			sor_i.setInputCloud(this->multiStationPointClouds[i]);
-			sor_i.setMeanK(50);
-			sor_i.setStddevMulThresh(this->getStdDev());
+    		sor_i.setInputCloud(dsed_rgb_cloud_i);
+    		sor_i.setMeanK(50);
+    		sor_i.setStddevMulThresh(this->getPreAlignStdDev());
 			sor_i.setNegative(false);
-			sor_i.filter(*mid_rgb_cloud_i);
-			if(this->getLeaf() == 0.0)
-			{
-				*dataLibrary::cloudxyzrgb += *mid_rgb_cloud_i;
-			}
-			else
-			{
-				pcl::VoxelGrid<pcl::PointXYZRGB> vg_i;
-				vg_i.setInputCloud (mid_rgb_cloud_i);
-				vg_i.setLeafSize (this->getLeaf(), this->getLeaf(), this->getLeaf());
-				vg_i.filter (*dsed_rgb_cloud_i);
-				*dataLibrary::cloudxyzrgb += *dsed_rgb_cloud_i;
-			}
+    		sor_i.filter(*ro_rgb_cloud_i);
+			pcl::NormalEstimationOMP<pcl::PointXYZRGB, pcl::Normal> ne_i;
+    		ne_i.setInputCloud(ro_rgb_cloud_i);
+    		pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree_i (new pcl::search::KdTree<pcl::PointXYZRGB>());
+    		ne_i.setSearchMethod(tree_i);
+    		ne_i.setKSearch(this->getPreAlignNormalsK());
+    		ne_i.compute(*src_normals);
+			pcl::concatenateFields(*ro_rgb_cloud_i, *src_normals, *src_pointnormal);
+			pcl::IterativeClosestPoint<pcl::PointXYZRGBNormal, pcl::PointXYZRGBNormal> icp;
+			icp.setInputCloud(src_pointnormal);
+			icp.setInputTarget(target_pointnormal);
+			icp.setMaxCorrespondenceDistance(this->getMaxCorrDistance());
+			icp.setEuclideanFitnessEpsilon(this->getEFEpsilon());
+			icp.align (*transformed_pointnormal);
+			//pcl::copyPointCloud(*transformed_pointnormal, *transformed_rgb_cloud_i);
+			pcl::transformPointCloud (*this->multiStationPointClouds[i], *transformed_rgb_cloud_i, icp.getFinalTransformation());
+			*dataLibrary::cloudxyzrgb += *transformed_rgb_cloud_i;
 		}
 
 		if(!dataLibrary::cloudxyz->empty())
